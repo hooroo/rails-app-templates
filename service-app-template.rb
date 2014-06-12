500d633d67fba8a951ef30b5d4444beddbee7dbb
Gabriel Rotbart <gabe@rotbart.net>
2014-06-12 14:35:36 +1000

    fixed some issues. removed debugger for byebug

--- service-app-template.rb
+++ service-app-template.rb
@@ -5,21 +5,21 @@ gem 'unicorn'
 gem 'hat',      git: 'git@github.com:hooroo/hooroo-api-tools.git'
 gem 'query_ar', git: 'git@github.com:hooroo/query_ar.git'
 gem 'stethoscope'
-gem 'http_client', git: 'git@github.com:hooroo/http_client.git'
+gem 'http_api_client', git: 'git@github.com:hooroo/http_api_client.git'
 gem 'togglr', git: 'git@github.com:hooroo/togglr.git'
 gem 'faraday'
 
 gem 's3_uri', git: 'git@github.com:hooroo/s3_uri.git'
 
-group_group :development do
+gem_group :development do
   gem 'foreman'
 end
 
 gem_group :development, :test do
   gem 'rspec-rails'
   gem 'pry'
-  gem 'pry-debugger' unless ENV['RUBYMINE_ZEUS'] || ENV['RM_INFO']
   gem 'pry-remote'
+  gem 'pry-byebug'
 end
 
 gem_group :test do
@@ -29,24 +29,44 @@ gem_group :test do
   gem 'mock_redis'
 end
 
-#generators
-generate('rspec:install')
-
 #Foreman Start Procfile
-run "echo 'web: bundle exec unicorn_rails --config-file config/unicorn.rb' >> Procfile"
-run "echo 'web_log: touch log/development.log log/unicorn.log ; tail -f log/development.log log/unicorn.log' >> Procfile"
+file 'Procfile', <<-PROCFILE
+  web: bundle exec unicorn_rails --config-file config/unicorn.rb
+  web_log: touch log/development.log log/unicorn.log ; tail -f log/development.log log/unicorn.log
+PROCFILE
+
+#rspec
+file '.rspec', <<-RSPEC
+  --color
+  --format documentation
+  --profile
+RSPEC
+
+#pact
+file 'spec/support/pact_helper.rb', <<-PACT
+  $LOAD_PATH << File.join(File.dirname(__FILE__),'../..','spec')
+  require 'spec_helper'
+  require 'api_clients'
+  require 'pact/consumer/rspec'
+
+  Pact.configure do | config |
+    config.pact_dir = "spec/integration/pacts"
+    config.logger.level = Logger::DEBUG
+    config.pactfile_write_mode = :overwrite
+  end
+PACT
 
 #gitignore
-run "cat << EOF >> .gitignore
-/.bundle
-/log/*.log
-/tmp
-database.yml
-doc/
-*.swp
-*~
-.DS_Store
-EOF"
+file '.gitignore', <<-GITIGNORE
+  /.bundle
+  /log/*.log
+  /tmp
+  database.yml
+  doc/
+  *.swp
+  *~
+  .DS_Store
+GITIGNORE
 
 #git
 git :init
